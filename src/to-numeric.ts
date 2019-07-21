44353fbcb04ea543bd944ec76e4e2bcd253c55f9
Yomotsu <admin@yomotsu.net>
2019-07-21 15:05:26 +0900

    better toNumeric

--- src/to-numeric.ts
+++ src/to-numeric.ts
@@ -4,10 +4,10 @@ export function toNumeric( value: string ): string {
 
 	const asciiString = toAscii( value )
 		.replace( /[^0-9.-]/g, '' )     // remove non numeric letters
-		.replace(/(?!^)-|[^-\d]/g, '' ) // --1-0 -> -10
+		.replace(/(?!^)-/g, '' )        // --1-0 -> -10
 		.replace( /\.+/, '.' )          // 0..0 -> 0.0
-		.replace( /^0+/, '0' )          // 000.000 -> 0.000
-		.replace( /^0([0-9])+/, '$1' )  // 01.000 -> 1.000
+		.replace( /^(-)?0+/, '$10' )     // 000.000 -> 0.000
+		.replace( /^(-)?0([1-9])+/, '$1$2' )  // 01.000 -> 1.000
 
 	const contains2MoreDot = /\..*\./.test( asciiString );
 	
